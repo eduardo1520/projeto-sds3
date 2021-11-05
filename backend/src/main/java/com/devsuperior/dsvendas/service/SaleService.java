347fde245e90b24876a720b7b022ec57217fa2b2
Eduardo Oliveira <emiranda.dev@gmail.com>
2021-11-05 13:04:06 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,9 +9,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
-import com.devsuperior.dsvendas.repositories.SallerRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -18,14 +22,24 @@ public class SaleService {
 	 private SaleRepository repository;
 	
 	@Autowired
-	private SallerRepository sallerRepository;
+	private SellerRepository sellerRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable) {
-		sallerRepository.findAll();
+		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 
 }
--- backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.entities.Seller;
-import com.devsuperior.dsvendas.repositories.SallerRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SellerService {
 	
 	@Autowired
-	private SallerRepository repository;
+	private SellerRepository repository;
 	
 	public List<SellerDTO> findAll() {
 		List<Seller> result = repository.findAll();
